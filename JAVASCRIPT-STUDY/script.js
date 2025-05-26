0336a894d9530f99cc0606e3165a857cef4a5d92
Songtaeha <songtaeha@naver.com>
2025-05-26 19:45:14 +0900

    프롬프트, 템플릿 리터럴

---
+++ JAVASCRIPT-STUDY/script.js
@@ -0,0 +1,22 @@
+const promptMessage = "프롬프트에 표시할 문자열";
+const result = prompt(promptMessage);
+console.log(result);
+
+let str = "따옴표 문자열";
+console.log(str);
+str = `백틱 문자열`;
+console.log(str)
+
+let number = 21;
+let player = "파울로 디발라"
+
+console.log(
+  `제가 좋아하는 축구선수는 ${player}, 그의 등번호는 ${number}`
+)
+
+let goat = prompt("세계 최고의 축구선수는?")
+let Number = prompt("그의 등번호는 ? ") 
+
+console.log(
+  `세계 최고의 축구선수는 ${goat}, 그의 등번호는 ${Number}`
+)
