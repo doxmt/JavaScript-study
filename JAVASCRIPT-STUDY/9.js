292e34ea44e77d2b38653d21a758c91294e8b3b5
Songtaeha <songtaeha@naver.com>
2025-05-31 18:25:24 +0900

    로컬스토리지

---
+++ JAVASCRIPT-STUDY/9.js
@@ -0,0 +1,15 @@
+// const myName = "송태하"
+// localStorage.setItem("myName", myName)
+
+
+const myName = localStorage.getItem("myName")
+
+alert(myName)
+
+// localStorage.setItem("cat", "고양이")
+// localStorage.setItem("dog", "강아지")
+// localStorage.setItem("myName", "songtaeha") 덮어쓰기됌
+
+// localStorage.removeItem("dog")
+
+// localStorage.clear()
